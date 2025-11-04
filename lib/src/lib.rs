360b93b2a29e60f63b23126df1551b4dc5165d24
Wasiries <wasiries.email@yandex.ru>
2025-11-04 13:13:18 +0300

    pre release version

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,44 +1,35 @@
-static mut IP: String = String::new();
+// use std::net::{TcpListener, TcpStream};
+// use std::thread;
+// use std::io::{Read, Write, Error};
 
-use std::net::{TcpListener, TcpStream};
-use std::thread;
-use std::io::{Read, Write, Error};
+// pub fn enter() -> String {
+//     let mut value = String::new();
+//     std::io::stdin().read_line(&mut value).unwrap();
+//     let value: String = value.trim().parse().unwrap();
+//     return value;
+// }
 
-pub fn enter() -> String {
-    let mut value = String::new();
-    std::io::stdin().read_line(&mut value).unwrap();
-    let value: String = value.trim().parse().unwrap();
-    return value;
-}
+// fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
+//     println!("Incoming connection from: {}", stream.peer_addr()?);
+//     let mut buf = [0; 1024];
+//     loop {
+//         let bytes_read = stream.read(&mut buf)?;
+//         if bytes_read == 0 { 
+//             return Ok(());
+//         }
+//         if &buf[..bytes_read] == "$exit$".as_bytes() {
+//             return Ok(());
+//         }
+//         let s = String::from_utf8_lossy(&buf[..bytes_read]);
+//         println!("{}", s);
+//     }
+// }
 
-fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
-    println!("Incoming connection from: {}", stream.peer_addr()?);
-    let mut buf = [0; 1024];
-    loop {
-        let bytes_read = stream.read(&mut buf)?;
-        if bytes_read == 0 { return Ok(()) }
-        stream.write(&buf[..bytes_read])?;
-    }
-}
+// pub fn initialize(ip_: String) -> Result<TcpListener, Box<dyn std::error::Error>> {
+//     let listener = TcpListener::bind(&ip_[..])?;
+//     return Ok(listener);
+// }
 
-pub fn initialize(ip_: String) {
-    unsafe {
-        IP = ip_;
-    }
-}
-
-pub fn begining() {
-    unsafe {
-        let listener = TcpListener::bind(&IP[..]).expect("Could not bind");
-        for stream in listener.incoming() {
-            match stream {
-                Err(e) => { eprintln!("failed: {}", e) }
-                Ok(stream) => {
-                    thread::spawn(move || {
-                        handle_client(stream).unwrap_or_else(|error| eprintln!("{:?}", error));
-                    });
-                }
-            }
-        }
-    }
-}
+// pub fn begining() {
+    
+// }
--- main/src/main.rs
+++ main/src/main.rs
@@ -1,6 +1,79 @@
-use lib;
+
+use std::{
+    net::{TcpListener, TcpStream},
+    thread,
+    io::{self, Read, Write},
+    sync::mpsc,
+};
+
+#[allow(dead_code)]
+fn encryption(value: &[u8]) -> Vec<u8> {
+    let ans = Vec::from(value);
+    return ans;
+}
+#[allow(dead_code)]
+fn decryption(value: &[u8]) -> Vec<u8> {
+
+    let ans = Vec::from(value);
+    return ans;
+}
+
+
 fn main() {
-    let ip = lib::enter();
-    lib::initialize(ip);
-    lib::begining();
+    let mut ip = String::new();
+    io::stdin().read_line(&mut ip).unwrap();
+    ip = ip.trim().parse().unwrap();
+
+    let listener = TcpListener::bind(&ip[..]).unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        let (sender, receiver) = mpsc::channel();
+        let (interruption_s, interruption_r) = mpsc::channel();
+        let closure = move || {
+            chating(sender, interruption_r);
+        };
+        let handle = thread::spawn(closure);
+        handle_client(stream, receiver);
+        let _ = interruption_s.send(true);
+        let _ = handle.join();
+    }
+}
+
+fn chating(sender: mpsc::Sender<String>, interruption: mpsc::Receiver<bool>) {
+    let mut value = String::new();
+    loop {
+        if let Ok(_) = interruption.try_recv() {
+            break;
+        }
+        io::stdin().read_line(&mut value).unwrap();
+        value = value.trim().parse().unwrap();
+        let _ = sender.send(value.clone());
+        value = String::new();
+    }
+}
+
+fn handle_client(mut stream: TcpStream, receiver: mpsc::Receiver<String>) {
+    let mut buffer: [u8; 2048] = [0; 2048];
+    loop {
+        if let Ok(value) = receiver.try_recv() {
+            let _ = stream.write(value.as_bytes());
+        }
+        match stream.read(&mut buffer) {
+            Ok(buffer_size) => {
+                if buffer_size == 0 {
+                    println!("Client has disconnected");
+                    break;
+                } else {
+                    let value = &buffer[..buffer_size];
+                    let message = String::from_utf8(decryption(value)).unwrap();
+                    println!("{}", message);
+                }
+            },
+            Err(e) => {
+                println!("Client processing error: {}", e.kind());
+                break;
+            }
+        }
+    }
 }
